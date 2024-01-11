6c8e134bacbab13bdfd1d6ccc9c3997341732edc
Devsinglad <109593493+Devsinglad@users.noreply.github.com>
2024-01-11 14:20:10 +0100

    manifest update

--- lib/screens/favorite/favorite_screen.dart
+++ lib/screens/favorite/favorite_screen.dart
@@ -11,45 +11,76 @@ class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Consumer<ProductApi>(builder: (context, snapshot, _) {
-        final favoriteProducts = snapshot.products
-            .where((product) => context.read<AppControllers>().isFavorite(product.id))
-            .toList();
-        return Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
-              Text(
-                "Favorites",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: GridView.builder(
-                    itemCount: favoriteProducts.length,
-                    gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                      maxCrossAxisExtent: 200,
-                      childAspectRatio: 0.7,
-                      mainAxisSpacing: 20,
-                      crossAxisSpacing: 16,
-                    ),
-                    itemBuilder: (context, index) => ProductCard(
-                        product: favoriteProducts[index],
-                        onPress: () => Navigator.push(context,
-                            MaterialPageRoute(builder: (_) =>  DetailsScreen(
-                              id: favoriteProducts[index].id.toString(),
-                              productsInEachCat:favoriteProducts[index] ,
-                              rating:favoriteProducts[index].rating.toString(),
 
-                            )))),
-                  ),
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          title: Text(
+          "Favorites",
+          style: Theme.of(context).textTheme.titleLarge,
+        ),),
+        body: Consumer<ProductApi>(builder: (context, snapshot, _) {
+          final favoriteProducts = snapshot.products
+              .where((product) =>
+                  context.read<AppControllers>().isFavorite(product.id))
+              .toList();
+          return Padding(
+            padding: const EdgeInsets.all(20.0),
+            child:favoriteProducts.isNotEmpty
+                ? Column(
+              children: [
+
+                 Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: GridView.builder(
+                            itemCount: favoriteProducts.length,
+                            gridDelegate:
+                                const SliverGridDelegateWithMaxCrossAxisExtent(
+                              maxCrossAxisExtent: 200,
+                              childAspectRatio: 0.7,
+                              mainAxisSpacing: 20,
+                              crossAxisSpacing: 16,
+                            ),
+                            itemBuilder: (context, index) => ProductCard(
+                                product: favoriteProducts[index],
+                                onPress: () => Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (_) => DetailsScreen(
+                                              id: favoriteProducts[index]
+                                                  .id
+                                                  .toString(),
+                                              productsInEachCat:
+                                                  favoriteProducts[index],
+                                              rating: favoriteProducts[index]
+                                                  .rating
+                                                  .toString(),
+                                            )))),
+                          ),
+                        ),
+                      )
+
+              ],
+            ): Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(child: Image.asset('assets/images/empty.png')),
+              const Text(
+                'No Items Added',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 17,
                 ),
-              )
+              ),
             ],
           ),
-        );
-      }),
+          );
+        }),
+      ),
     );
   }
 }
